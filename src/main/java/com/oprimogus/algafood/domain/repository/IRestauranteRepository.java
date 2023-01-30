f3a10a122bd4289bb7faf2ff4ee74fb4e3bdf379
oprimogus <gustavo.jesus@proton.me>
2023-01-29 21:02:33 -0300

    merge desktop in main

--- src/main/java/com/oprimogus/algafood/domain/repository/IRestauranteRepository.java
+++ src/main/java/com/oprimogus/algafood/domain/repository/IRestauranteRepository.java
@@ -2,6 +2,7 @@ package com.oprimogus.algafood.domain.repository;
 
 import com.oprimogus.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
