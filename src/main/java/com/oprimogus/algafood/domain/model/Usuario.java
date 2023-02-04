359029e4163d7d6a6f730e1634a09f978c9a7aa9
oprimogus <gustavo.jesus@proton.me>
2023-02-04 16:23:38 -0300

    git add Usuario model

---
+++ src/main/java/com/oprimogus/algafood/domain/model/Usuario.java
@@ -0,0 +1,41 @@
+package com.oprimogus.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false, columnDefinition = "datetime")
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+    @ManyToMany
+    @JoinTable(name = "usuario_grupo",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+    private List<Grupo> grupos = new ArrayList<>();
+
+
+}
