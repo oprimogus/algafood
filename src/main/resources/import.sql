aee79807f4e7709f9f9fb2b3aab612a520aff147
oprimogus <gustavo.jesus@proton.me>
2023-02-03 23:17:56 -0300

    ignoring application.yml

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,8 +5,6 @@ insert into estados (id, nome) values (3, 'Ceará');
 insert into cidades (id, nome, estado_id) values (1, 'Uberlândia', 1);
 insert into cidades (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
 insert into cidades (id, nome, estado_id) values (3, 'São Paulo', 2);
-insert into cidades (id, nome, estado_id) values (4, 'Campinas', 2);
-insert into cidades (id, nome, estado_id) values (5, 'Fortaleza', 3);
 
 insert into cozinhas (id, nome) values (1, 'Brasileira');
 insert into cozinhas (id, nome) values (2, 'Americana');
